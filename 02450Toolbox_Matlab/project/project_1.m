a03e330a733bfc5e101aa7f2abeabb6bfdf05557
Victor Alexander Hansen <66786672+Vict0rAH@users.noreply.github.com>
2021-09-21 11:54:22 +0200

    Update project_1.m

--- 02450Toolbox_Matlab/project/project_1.m
+++ 02450Toolbox_Matlab/project/project_1.m
@@ -12,8 +12,16 @@ file_path = fullfile(cdir,'../project/Data_abalone_dot.csv');
 abalone_table = readtable(file_path);
 X = table2array(abalone_table(:, 2:8));
 
-% Mean values for attributes
-% What to do with the nominal?
-mean(X(1:end,2))
+% summary statistics for numerical attributes
+S = {'Length','Diameter','Height','Whole weight','Shucked weight','Viscera weight','Shell weight','Rings'};
+for i = 1:8
+    S(i)
+    mean(X(1:end,i))
+    quantile(X(1:end,i),[0 0.25 0.5 0.75 1])
+    var(X(1:end,i))
+    std(X(1:end,i))
+end
 
-%Sample text
+% What to do with the nominal?
+mode(categorical(table2cell(abalone_table(:,1))))
+% Sample text
